d79172215c8ec6148be2ce21f99b562a59088ba3
Ruan Rocha <ruanrochaeuok@gmail.com>
2025-04-29 18:27:42 -0300

    Arrumando nome do arquivo

--- Aula1.py
+++ Aula1.py
@@ -48,9 +48,9 @@ parte_inteira, parte_decimal = num_str.split('.')
 parte_decimal_int = int(parte_decimal[2])
 parte_decimal_segunda = int(parte_decimal[1])
 if parte_decimal_int >= 5:
-    num_str = f"{parte_inteira}.{parte_decimal[0]}{parte_decimal_segunda +1}
+    num_str = f"{parte_inteira}.{parte_decimal[0]}{parte_decimal_segunda +1}"
 
-
+print(f"A resposta da questão 4 é:{num_str}")
 
 
 
