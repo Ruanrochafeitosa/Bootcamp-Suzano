6de654a7935457856a9a333026e4a22d85832851
Ruan Rocha <ruanrochaeuok@gmail.com>
2025-04-29 18:01:30 -0300

    Testando recuperação

--- Aula 1.py
+++ Aula 1.py
@@ -50,7 +50,7 @@ parte_decimal_segunda = int(parte_decimal[1])
 if parte_decimal_int >= 5:
     num_str = f"{parte_inteira}.{parte_decimal[0]}{parte_decimal_segunda +1}"
 
-print(f"A resposta da questão 4 é:{num_str}")
+
 
 
 
